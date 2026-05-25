95dace78e7e2edbc02c774d7dff5d746ae178a8a
panggilardra <godamlaskarbesi@gmail.com>
2026-05-25 23:34:11 +0700

    placeovenimage

--- Room/RoomBaking.py
+++ Room/RoomBaking.py
@@ -1,11 +1,12 @@
 import pygame, os
 from Room.Room import Room
-from Constant import SCREEN_WIDTH, SCREEN_HEIGHT, BAKING_BG, COLOR_BG_CREAM, COLOR_DARK_BROWN, FONT_HEADING_SIZE, FONT_BODY_SIZE, FONT_NAME
+from Constant import SCREEN_WIDTH, SCREEN_HEIGHT, BAKING_BG, COLOR_BG_CREAM, COLOR_DARK_BROWN, FONT_HEADING_SIZE, FONT_BODY_SIZE, FONT_NAME, OVEN_CLOSE_IMAGE
 
 class BakingRoom(Room):
     def __init__(self):
         super().__init__(name="Baking")
         self._bg_image = None
+        self._oven_image = None
         self._font_heading = pygame.font.SysFont(FONT_NAME, FONT_HEADING_SIZE)
         self._font_body = pygame.font.SysFont(FONT_NAME, FONT_BODY_SIZE)
 
@@ -13,6 +14,15 @@ class BakingRoom(Room):
         if os.path.exists(BAKING_BG):
             self._bg_image = pygame.transform.smoothscale(
                 pygame.image.load(BAKING_BG).convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+          # Load oven image
+        if os.path.exists(OVEN_CLOSE_IMAGE):
+            img = pygame.image.load(OVEN_CLOSE_IMAGE).convert_alpha()  # convert_alpha untuk transparency
+            # Scale sesuai kebutuhan, misal jadi lebar 200px dengan aspect ratio tetap
+            original_width, original_height = img.get_size()
+            new_width = 520
+            new_height = int(original_height * (new_width / original_width))
+            self._oven_image = pygame.transform.smoothscale(img, (new_width, new_height))
+
     def update(self): pass
 
     def render(self):
@@ -24,6 +34,9 @@ class BakingRoom(Room):
             t = self._font_heading.render("~ Cashier ~", True, COLOR_DARK_BROWN)
             self.screen.blit(t, t.get_rect(centerx=SCREEN_WIDTH//2, centery=SCREEN_HEIGHT//3))
 
-    def handle_event(self, event): pass
+        if self._oven_image:
+            oven_rect = self._oven_image.get_rect(centerx=SCREEN_WIDTH - 550,
+                bottom=SCREEN_HEIGHT + 5)
+            self.screen.blit(self._oven_image, oven_rect)
 
-    
+    def handle_event(self, event): pass
